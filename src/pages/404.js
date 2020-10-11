9710bc1e1654f1ab5239dbc1bfb70918a6afb2e3
Aaron <ilestkempo@gmail.com>
2020-10-11 14:56:16 -0700

    added favicon and 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+function NotFound() {
+  return (
+    <Layout>
+      <h1>Whoops!</h1>
+      <p>Looks like no page exists here.</p>
+    </Layout>
+  )
+}
+
+export default NotFound;
