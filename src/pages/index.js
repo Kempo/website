850152b0dc2cf50d332014969fc00cdf49bc79b1
Aaron <ilestkempo@gmail.com>
2020-11-02 18:01:43 -0800

    update frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,9 @@ function Home({ data }) {
           <li><a href="https://www.goodreads.com/book/show/904538.The_Book_of_Tea">The Book of Tea</a></li>
           <li><a href="https://www.goodreads.com/book/show/6321411-something-incredibly-wonderful-happens">Something wonderfully incredible happens</a></li>
           <li><a href="https://www.goodreads.com/book/show/52838315-greenlights">Greenlights (Matthew McConaughey)</a></li>
+          <li><a href="https://www.goodreads.com/book/show/10483171-the-beginning-of-infinity">The Beginning of Infinity (David Deutsch)</a></li>
+          <li><a href="https://www.goodreads.com/book/show/37004370-the-memory-police">The Memory Police (Yoko Ogawa)</a></li>
+          <li><a href="https://www.goodreads.com/book/show/36064445-skin-in-the-game">Skin in the Game (Nassim Taleb)</a></li>
         </ul>
       </div>
     </Layout>
