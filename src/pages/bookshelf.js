5249895d6a422ad5908168fadc0e0b7e961d7f2e
Aaron <ilestkempo@gmail.com>
2021-05-28 09:34:07 -0700

    bookshelf

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -54,6 +54,7 @@ function Books() {
         <li><a href="http://paulgraham.com/worked.html">What I worked on (Paul Graham)</a></li>
         <li><a href="https://wired.com/story/lee-holloway-devastating-decline-brilliant-young-coder/">What Happened to Lee Holloway</a></li>
         <li><a href="https://www.amazon.com/Scout-Mindset-People-Things-Clearly-ebook/dp/B07L2HQ26K">The Scout Mindset (Julia Galef)</a></li>
+        <li><a href="https://www.goodreads.com/book/show/144409.From_Third_World_to_First"><b>From Third World to First (Lee Kuan Yew)</b></a></li>
       </ul>
       <p><strong>Artists</strong></p>
       <p>People I admire. </p>
