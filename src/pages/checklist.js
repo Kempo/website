47526dbfeb2379f789e8d3a85d7d3177b87eae80
Aaron <ilestkempo@gmail.com>
2021-11-22 00:27:44 -0800

    update checklist

--- src/pages/checklist.js
+++ src/pages/checklist.js
@@ -15,6 +15,17 @@ export default function Checklist() {
         <li>Max Richter concert</li>
         <li>Cannes / Sundance Film Festival</li>
         <li>Be a film extra</li>
+        <li>Hikaru Museum</li>
+        <li>Mulholland Drive</li>
+        <li>Hollyhock House</li>
+        <li>NYC Comedy Club (Comedy Cellar, etc)</li>
+        <li>Eat pinsa</li>
+        <li>Learn a style of dancing (salsa?)</li>
+        <li>Get the materials for a hoodie and sew it from scratch</li>
+        <li>Write a book</li>
+        <li>Learn how to make ceramics (ie. cups)</li>
+        <li>Thrifting in Shibuya</li>
+        <li>Try true Texas barbeque</li>
       </ul>
     </Layout>
   )
