5a12acf338d2b65e6c2a3d8341e6b36017bf770d
Aaron <ilestkempo@gmail.com>
2022-01-28 11:46:36 -0800

    add book

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -63,6 +63,7 @@ function Books() {
         <li><a href="https://moxie.org/stories.html">Stories of Life (Moxie Marlinspike)</a></li>
         <li><a href="https://www.goodreads.com/book/show/629.Zen_and_the_Art_of_Motorcycle_Maintenance">Zen and the Art of Motorcycle Maintenance</a></li>
         <li><a href="https://www.goodreads.com/book/show/18774964-a-man-called-ove">A Man Called Ove</a></li>
+        <li><a href="https://www.goodreads.com/book/show/40603783-the-defining-decade">Defining the Decade</a></li>
       </ul>
       <p><strong>Artists</strong></p>
       <p>People I admire. </p>
