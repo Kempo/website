ff645d5c608751fb52cab9d7cd4e2c421e3aa01f
Aaron Chen <aaron@moderntreasury.com>
2024-06-22 14:32:45 -0700

    Add last updated at

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,70 @@
-import React from "react";
-import { graphql } from 'gatsby';
-import Layout from '../components/layout';
-import SEO from '../components/SEO';
-import { StaticImage } from "gatsby-plugin-image";
-import { GatsbyImage } from "gatsby-plugin-image";
-import './index.scss';
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/SEO"
+import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage } from "gatsby-plugin-image"
+import "./index.scss"
 
 function Home({ data }) {
   return (
-    <Layout>  
+    <Layout>
       <SEO />
       <div style={{ marginBottom: 24 }}>
-        <p><strong>Me</strong></p>
-        <div className="profile">
-          <StaticImage 
-          src="../assets/me_3.jpg" 
-          alt="Myself"       
-          placeholder="blurred"
-          layout="fixed"
-          width={275} />
-        </div>
-        <p style={{ maxWidth: 500, width: '100%' }}> 
-          Hey! My name's Aaron. I code a bit, hoop a little, and (<i>try to</i>) read often.
-          On my free time, I cross up neighborhood kids at the court and love watching confusing movies. I also only ever cook Italian food.
+        <p>
+          <strong>Me</strong>
         </p>
-        <a href="https://twitter.com/tweetsbychen" style={{ marginRight: 12 }}>Twitter</a>
-        <a href="mailto:ilestkempo@gmail.com" style={{ marginRight: 12 }}>Email</a>
+        <div className="profile">
+          <StaticImage
+            src="../assets/me_3.jpg"
+            alt="Myself"
+            placeholder="blurred"
+            layout="fixed"
+            width={275}
+          />
+        </div>
+        <p style={{ maxWidth: 500, width: "100%" }}>
+          Hey! My name's Aaron. I code a bit, hoop a little, and (<i>try to</i>)
+          read often. On my free time, I cross up neighborhood kids at the court
+          and love watching confusing movies. I also only ever cook Italian
+          food.
+        </p>
+        <a href="https://twitter.com/tweetsbychen" style={{ marginRight: 12 }}>
+          Twitter
+        </a>
+        <a href="mailto:ilestkempo@gmail.com" style={{ marginRight: 12 }}>
+          Email
+        </a>
         <a href="https://github.com/kempo">Github</a>
+        <p style={{ marginBottom: 0, color: "#adadad", fontSize: "12px" }}>
+          <i>Last Updated June 2024</i>
+        </p>
         <br />
         <br />
         <GatsbyImage
           image={data.Kobe.childImageSharp.gatsbyImageData}
-          alt="Michael Jordan guarding Kobe Bryant" />
-          <br />
-          <GatsbyImage
+          alt="Michael Jordan guarding Kobe Bryant"
+        />
+        <br />
+        <GatsbyImage
           image={data.Taipei.childImageSharp.gatsbyImageData}
-          alt="Fujifilm scene from 'Taipei Story'" />
-          <br />
-          <GatsbyImage
+          alt="Fujifilm scene from 'Taipei Story'"
+        />
+        <br />
+        <GatsbyImage
           image={data.Dustballs.childImageSharp.gatsbyImageData}
-          alt="Dustballs from Spirited Away" />
-          <br />
+          alt="Dustballs from Spirited Away"
+        />
+        <br />
         <GatsbyImage
           image={data.Mood.childImageSharp.gatsbyImageData}
-          alt="Tony Leung in 'In the Mood for Love'" />
-          <br />
+          alt="Tony Leung in 'In the Mood for Love'"
+        />
+        <br />
         <GatsbyImage
           image={data.Cigarettes.childImageSharp.gatsbyImageData}
-          alt="Cigarettes scene in 'Drive My Car'" />
+          alt="Cigarettes scene in 'Drive My Car'"
+        />
         {/*
         <p>Stuff I like (a brief list).</p>
         <ul>
@@ -62,41 +79,40 @@ function Home({ data }) {
           <li><a href="https://www.rottentomatoes.com/m/dune_2021">Dune (movie)</a></li>
           <li><a href="https://www.rottentomatoes.com/m/nocturnal_animals">Nocturnal Animals (movie)</a></li>
   </ul>*/}
-
       </div>
     </Layout>
-  );
+  )
 }
 
-export const query = graphql`{
-  Dustballs: file(relativePath: {eq: "spirited-away-dust.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
+export const query = graphql`
+  {
+    Dustballs: file(relativePath: { eq: "spirited-away-dust.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+    Kobe: file(relativePath: { eq: "kobe.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+    Mood: file(relativePath: { eq: "in-the-mood-for-love.jpeg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+    Cigarettes: file(relativePath: { eq: "cigarettes.jpeg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+    Taipei: file(relativePath: { eq: "taipei-story.png" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
     }
   }
-  Kobe: file(relativePath: {eq: "kobe.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-    }
-  }
-  Mood: file(relativePath: {eq: "in-the-mood-for-love.jpeg"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-    }
-  }
-  Cigarettes: file(relativePath: {eq: "cigarettes.jpeg"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-    }
-  }
-  Taipei: file(relativePath: {eq: "taipei-story.png"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-    }
-  }
-}
-`;
-
+`
 
 // export const query = graphql`
 //   query FetchPosts {
@@ -115,4 +131,4 @@ export const query = graphql`{
 //   }
 // `;
 
-export default Home;
+export default Home
